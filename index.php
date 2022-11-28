de1c29668e51efa3afff65fb82143a4e155ee8c1
MaTtiaa01 <mattia2001@live.it>
2022-11-28 15:57:44 +0100

    print in column

--- index.php
+++ index.php
@@ -49,17 +49,33 @@ $hotels = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>hotel</title>
 </head>
 
 <body>
     <div class="container">
-        <h1>hotel list</h1>
+        <h1 class="text-center text-uppercase">hotel list</h1>
 
-        <?php foreach ($hotels as $hotel)
-            echo $hotel['name'] . '<br>' . $hotel['description'] . '<br>' . $hotel['vote'] . '<br>' . $hotel['distance_to_center'] . '<br>';
-
-        ?>
+        <?php foreach ($hotels as $hotel) : ?>
+            <div class="row">
+                <div class="col border">
+                    <?= $hotel['name']; ?>
+                </div>
+                <div class="col border">
+                    <?= $hotel['description']; ?>
+                </div>
+                <div class="col border">
+                    <?= $hotel['parking']; ?>
+                </div>
+                <div class="col border">
+                    <?= $hotel['vote']; ?>
+                </div>
+                <div class="col border">
+                    <?= $hotel['distance_to_center']; ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
 
 
     </div>
